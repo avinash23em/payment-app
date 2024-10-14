9585b3dcc6267902adb8f8c84beee09f31b41b81
avinash23em <ap23vi@gmail.com>
2024-10-14 19:51:47 +0530

    intial

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,50 @@
 const express= require("express");
-const userRouter=require("/user")
+const { User } = require("../db");
+const zod=require("zod")
+const jwt=require("jsonwebtoken")
+const userRouter=require("./user");
+const JWT_SECRET = require("../config");
 
 const router=express.Router();
+const signupSchema=zod.object({
+    username:zod.string().email(),
+    firstname:zod.string(),
+    lastname:zod.string(),
+    password:zod.string()
+})
 
- router.use("/user",userRouter);
+ router.post("/signup",async (req,res)=>{
+    const body=req.body;
+const {sucess}=signupSchema.safeParse(req.body);
+if(!sucess){
+     return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
+})
+}
+const existuser= await User.findOne({
+username: body.username
+})
+if(existuser){
+    return res.status(411).json({
+        message: "Email already taken/Incorrect inputs"
+    })
+}
+const user=await User.create({
+    username:body.username,
+    firstname:body.firstname,
+    lastname:body.lastname,
+    password:body.password
+})
+const userid=user._id;
+const token=jwt.sign({
+    userid
+},JWT_SECRET)
+res.json({
+    message:"user created succesfully",
+    token:token
+})
+
+ })
 
 module.exports=router;
 
